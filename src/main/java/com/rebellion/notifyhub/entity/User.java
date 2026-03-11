2e2b49e655267164dad00570b247b98b1c1e3e30
avinashee0012 <avinashee0012@gmail.com>
2026-03-11 22:34:28 +0530

    feat: add reverse mapping to user

--- src/main/java/com/rebellion/notifyhub/entity/User.java
+++ src/main/java/com/rebellion/notifyhub/entity/User.java
@@ -1,10 +1,14 @@
 package com.rebellion.notifyhub.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 
@@ -22,4 +26,10 @@ public class User extends BaseEntity {
 
     @Column(nullable = false)
     private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Notification> notifications;
+
+    @OneToOne(mappedBy = "user")
+    private NotificationPreference preference;
 }
