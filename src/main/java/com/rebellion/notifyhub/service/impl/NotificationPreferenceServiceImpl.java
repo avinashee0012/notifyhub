4ab233b5435adab86967ce2c34a148b6b3c00ca5
avinashee0012 <avinashee0012@gmail.com>
2026-03-12 22:18:38 +0530

    feat: make update preference transactional

--- src/main/java/com/rebellion/notifyhub/service/impl/NotificationPreferenceServiceImpl.java
+++ src/main/java/com/rebellion/notifyhub/service/impl/NotificationPreferenceServiceImpl.java
@@ -6,10 +6,10 @@ import com.rebellion.notifyhub.dto.request.PreferenceUpdateDto;
 import com.rebellion.notifyhub.dto.response.PreferenceResponseDto;
 import com.rebellion.notifyhub.entity.NotificationPreference;
 import com.rebellion.notifyhub.repository.NotificationPreferenceRepo;
-import com.rebellion.notifyhub.repository.UserRepo;
 import com.rebellion.notifyhub.service.NotificationPreferenceService;
 
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -17,19 +17,17 @@ import lombok.RequiredArgsConstructor;
 public class NotificationPreferenceServiceImpl implements NotificationPreferenceService {
 
     private final NotificationPreferenceRepo notificationPreferenceRepo;
-    private final UserRepo userRepo;
 
     @Override
     public PreferenceResponseDto getPreferences(Long userId) {
-        if(!userRepo.existsById(userId)) throw new EntityNotFoundException("User not found");
-        NotificationPreference preference = notificationPreferenceRepo.findByUserId(userId).orElseThrow(() -> new EntityNotFoundException("Preferences not found"));
+        NotificationPreference preference = notificationPreferenceRepo.findByUserId(userId).orElseThrow(() -> new EntityNotFoundException("Preference not found"));
         return new PreferenceResponseDto(preference.isEmailEnabled(), preference.isPushEnabled());
     }
 
     @Override
+    @Transactional
     public PreferenceResponseDto updatePreferences(Long userId, PreferenceUpdateDto request) {
-        if(!userRepo.existsById(userId)) throw new EntityNotFoundException("User not found");
-        NotificationPreference preference = notificationPreferenceRepo.findByUserId(userId).orElseThrow(() -> new EntityNotFoundException("Preferences not found"));
+        NotificationPreference preference = notificationPreferenceRepo.findByUserId(userId).orElseThrow(() -> new EntityNotFoundException("Preference not found"));
         preference.updateEmailPreference(request.isEmailEnabled());
         preference.updatePushPreference(request.isPushEnabled());
         notificationPreferenceRepo.save(preference);
