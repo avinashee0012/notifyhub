147b4973eb5cd33ecf8027b081ccb7df5952bbd7
avinashee0012 <avinashee0012@gmail.com>
2026-03-12 20:09:27 +0530

    refactor: remove unnecessary lombok annotations from ApiResponseDto

--- src/main/java/com/rebellion/notifyhub/dto/ApiResponseDto.java
+++ src/main/java/com/rebellion/notifyhub/dto/ApiResponseDto.java
@@ -1,14 +1,10 @@
 package com.rebellion.notifyhub.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
-@EqualsAndHashCode
-@ToString
 public class ApiResponseDto<T> {
     private final boolean success;
     private final T data;
