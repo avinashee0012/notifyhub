842443efe19fd86cc3f0bf8f86efdb0409b9f22e
avinashee0012 <avinashee0012@gmail.com>
2026-03-16 19:57:32 +0530

    feat: add MethodArgumentNotValidException to exception handler

--- src/main/java/com/rebellion/notifyhub/exception/GlobalExceptionHandler.java
+++ src/main/java/com/rebellion/notifyhub/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.rebellion.notifyhub.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,6 +10,13 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex, HttpServletRequest request){
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        ErrorResponseDto response = new ErrorResponseDto(status, ex.getMessage(), request.getPathInfo());
+        return ResponseEntity.status(status).body(response);
+    }
     
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponseDto> handleException(Exception ex, HttpServletRequest request){
