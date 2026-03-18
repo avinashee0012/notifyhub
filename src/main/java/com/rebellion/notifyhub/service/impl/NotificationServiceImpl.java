044528d928f8053555336d9faa5d1126dc421dd0
avinashee0012 <avinashee0012@gmail.com>
2026-03-18 22:03:11 +0530

    feat: add processNotification method to  notification service

--- src/main/java/com/rebellion/notifyhub/service/impl/NotificationServiceImpl.java
+++ src/main/java/com/rebellion/notifyhub/service/impl/NotificationServiceImpl.java
@@ -1,19 +1,26 @@
 package com.rebellion.notifyhub.service.impl;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.rebellion.notifyhub.entity.Notification;
 import com.rebellion.notifyhub.entity.NotificationEvent;
+import com.rebellion.notifyhub.entity.NotificationPreference;
 import com.rebellion.notifyhub.entity.User;
+import com.rebellion.notifyhub.entity.enums.NotificationStatus;
 import com.rebellion.notifyhub.repository.NotificationRepo;
 import com.rebellion.notifyhub.repository.UserRepo;
 import com.rebellion.notifyhub.service.NotificationService;
 
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class NotificationServiceImpl implements NotificationService{
 
 	private final UserRepo userRepo;
@@ -24,5 +31,48 @@ public class NotificationServiceImpl implements NotificationService{
 		User user = userRepo.findById(event.getUserId()).orElseThrow(() -> new EntityNotFoundException("User not found"));
 		Notification notification = new Notification(user, "New Event: " + event.getEventType(), event.getPayload(), event.getEventType());
 		notificationRepo.save(notification);
+	}
+
+	@Override
+	@Transactional
+	public void processPendingNotifications() {
+		List<Notification> notifications = notificationRepo.findByStatus(NotificationStatus.PENDING);
+		for (Notification notification : notifications) {
+			try {
+				processNotification(notification);
+				notification.markSent();
+			} catch (Exception e) {
+				log.info("Unable to send notification to " + notification.getUser().getEmail());
+				notification.markFailed();
+			}
+		}
+	}
+
+	// HELPER METHODS
+	private void processNotification(Notification notification) {
+		NotificationPreference preference = notification.getUser().getPreference();
+		if(preference == null){
+			throw new EntityNotFoundException("Preference not found");
+		}
+		if(preference.isEmailEnabled()) sendEmail(notification);
+		if(preference.isPushEnabled()) sendPush(notification);
+	}
+
+	private void sendEmail(Notification notification) {
+		// TODO replace with email service
+		log.info(
+			"Sending email to {}: {}",
+			notification.getUser().getEmail(),
+			notification.getMessage()
+		);
+	}
+
+	private void sendPush(Notification notification) {
+		// TODO replace with websocket push
+		log.info(
+			"Sending push to userId {}: {}",
+			notification.getUser().getId(),
+			notification.getMessage()
+		);
 	}
 }
