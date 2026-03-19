82cf336898880cc0a45f51732af6abec1995f59e
avinashee0012 <avinashee0012@gmail.com>
2026-03-19 19:05:34 +0530

    feat: add and implement methods to NotificationService
    paginated getNotifications(Long userId), getUnreadNotifications(Long userId), markAsRead(Long notificationId)
    
    implement methods and add toResponseDto mapper method

--- src/main/java/com/rebellion/notifyhub/service/NotificationService.java
+++ src/main/java/com/rebellion/notifyhub/service/NotificationService.java
@@ -1,9 +1,19 @@
 package com.rebellion.notifyhub.service;
 
 import com.rebellion.notifyhub.entity.NotificationEvent;
+import com.rebellion.notifyhub.dto.response.NotificationResponseDto;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface NotificationService {
 	void createNotification(NotificationEvent event);
 
     void processPendingNotifications();
+
+	Page<NotificationResponseDto> getNotifications(Long userId, Pageable pageable);
+
+	Page<NotificationResponseDto> getUnreadNotifications(Long userId, Pageable pageable);
+
+	NotificationResponseDto markAsRead(Long notificationId);
 }
