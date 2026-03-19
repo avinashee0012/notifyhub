df7bbc8016fbfc1ffddfafb90407eb873b7677b9
avinashee0012 <avinashee0012@gmail.com>
2026-03-19 19:02:19 +0530

    feat: improve NotificationRepo
    add paginated findByUserId and findByUserIdAndStatus methods

--- src/main/java/com/rebellion/notifyhub/repository/NotificationRepo.java
+++ src/main/java/com/rebellion/notifyhub/repository/NotificationRepo.java
@@ -1,14 +1,20 @@
 package com.rebellion.notifyhub.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import com.rebellion.notifyhub.entity.Notification;
 import com.rebellion.notifyhub.entity.enums.NotificationStatus;
 
+import java.util.List;
+
 @Repository
 public interface NotificationRepo extends JpaRepository<Notification, Long>{
     List<Notification> findByStatus(NotificationStatus pending);
+
+	Page<Notification> findByUserId(Long userId, Pageable pageable);
+
+	Page<Notification> findByUserIdAndStatus(Long userId, NotificationStatus status, Pageable pageable);
 }
